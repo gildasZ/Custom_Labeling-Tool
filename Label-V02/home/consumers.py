ccdc83c7c69b70049386716b11fd8cb9f8b023b2
gildasZ <gildast3@gmail.com>
2024-06-10 21:59:14 +0800

    Update 20240608

--- Label-V02/home/consumers.py
+++ Label-V02/home/consumers.py
@@ -31,6 +31,7 @@ class ECGConsumer(AsyncWebsocketConsumer):
         logger.info(f"\nDjango Received data through WebSocket: {text_data}.\n")
         data = json.loads(text_data)
 
+        #______________________________________________________________________________
         if data['type'] == 'processXML':
             # Sending message to Pipe in DjangoDash #####################################################################
             Data_to_Send = {'File-path': None, 'Channel': None}
@@ -60,6 +61,7 @@ class ECGConsumer(AsyncWebsocketConsumer):
                         label = 'All-Channels',  # Fixed label for the second pipe
                         value = {'all_channels': response['channels']})
         
+        #______________________________________________________________________________
         elif data['type'] == 'DashDisplayWaveform':
             path_variable = convert_path(html.unescape(data['fullFilePath']))
             logger.info(f"\nDjango received \n-full file path: {path_variable} \n-and selected channel: {data['channel']}\n")
@@ -72,53 +74,58 @@ class ECGConsumer(AsyncWebsocketConsumer):
                         value = Data_to_Send)
             logger.info(f"\n+++++ Django sent Message Channel data to ppd.Pipe: {Data_to_Send}")
 
+        #______________________________________________________________________________
+        elif data['type'] == 'Refresh_Save_Undo':
+            logger.info(f"\nDjango received \n\t-action_var: {data['Action_var']}\n")
+            
+        #______________________________________________________________________________
         else:
             logger.error(f"\nUnknown message type received: {data['type']}\n")
             await self.send(text_data=json.dumps({'error': 'Unknown message type'}))
 
     async def retrieved_data(self, event):
-            # This method is called when a message of type 'retrieved_data' is sent to the group
-            existing_Data = event['Existing_Data']
-            logger.info(f"\n+++++ Django Received Retrieved Data: \n\t\tExisting_Data: {existing_Data}\n\n")
+        # This method is called when a message of type 'retrieved_data' is sent to the group
+        existing_Data = event['Existing_Data']
+        logger.info(f"\n+++++ Django Received Retrieved Data: \n\t\tExisting_Data: {existing_Data}\n\n")
 
-            # Send the retrieved data to the client
-            await self.send(text_data=json.dumps({
-                'type': 'DjangoDash_retrieved_data_message',
-                'Existing_Data': existing_Data,
-            }))
-            logger.info(f"\n----- Django sent retrieved_data to the client: \n{existing_Data}\n")
+        # Send the retrieved data to the client
+        await self.send(text_data=json.dumps({
+            'type': 'DjangoDash_retrieved_data_message',
+            'Existing_Data': existing_Data,
+        }))
+        logger.info(f"\n----- Django sent retrieved_data to the client: \n{existing_Data}\n")
 
     async def form_submission(self, event):
-            # This method is called when a message of type 'form_submission' is sent to the group
-            annotation = event['annotation']
-            click_indices = event.get('click_indices', ['Start_index', 'End_index'])
-            item_number = event['item_number']
-            logger.info(f"\n+++++ Django Received form submission: \n-annotation: {annotation} \n-click indices: {click_indices} \n-and item_number: {item_number}\n")
+        # This method is called when a message of type 'form_submission' is sent to the group
+        annotation = event['annotation']
+        click_indices = event.get('click_indices', ['Start_index', 'End_index'])
+        item_number = event['item_number']
+        logger.info(f"\n+++++ Django Received form submission: \n-annotation: {annotation} \n-click indices: {click_indices} \n-and item_number: {item_number}\n")
 
-            # Extract annotation data
-            annotation_data = {
-                'Start Index': click_indices[0] if len(click_indices) > 0 else '',
-                'End Index': click_indices[1] if len(click_indices) > 1 else '',
-                'Label': annotation,
-                'Color': '#FF6347'  # You can adjust this to be dynamic if needed
-            }
-    
-            # # Assuming these values are available
-            # full_file_path = "full/path/to/xml/file.xml"  # Must receive it from DjangoDash
-            # channels = ['Channel1', 'Channel2', 'Channel3']  # Must receive it from DjangoDash
-            # selected_channel = 'Channel1'  # Must receive it from DjangoDash Or create a variable that get updated in another method
-            
-            # # Add annotation to CSV
-            # item_number = add_annotation_to_csv(full_file_path, channels, selected_channel, annotation_data)
+        # # Extract annotation data
+        # annotation_data = {
+        #     'Start Index': click_indices[0] if len(click_indices) > 0 else '',
+        #     'End Index': click_indices[1] if len(click_indices) > 1 else '',
+        #     'Label': annotation,
+        #     'Color': '#FF6347'  # You can adjust this to be dynamic if needed
+        # }
 
-            # Send the annotation as a response to the client
-            await self.send(text_data=json.dumps({
-                'type': 'DjangoDash_message',
-                'Annotation_message': annotation,
-                'Click_indices': click_indices,
-                'Item_number': item_number,
-            }))
-            logger.info(f"\n----- Django sent form submission annotation, click indices and item number to the client: \n{annotation}, \n{click_indices}, \n{item_number}\n")
+        # # Assuming these values are available
+        # full_file_path = "full/path/to/xml/file.xml"  # Must receive it from DjangoDash
+        # channels = ['Channel1', 'Channel2', 'Channel3']  # Must receive it from DjangoDash
+        # selected_channel = 'Channel1'  # Must receive it from DjangoDash Or create a variable that get updated in another method
+        
+        # # Add annotation to CSV
+        # item_number = add_annotation_to_csv(full_file_path, channels, selected_channel, annotation_data)
+
+        # Send the annotation as a response to the client
+        await self.send(text_data=json.dumps({
+            'type': 'DjangoDash_message',
+            'Annotation_message': annotation,
+            'Click_indices': click_indices,
+            'Item_number': item_number,
+        }))
+        logger.info(f"\n----- Django sent form submission annotation, click indices and item number to the client: \n{annotation}, \n{click_indices}, \n{item_number}\n")
 
 
 
